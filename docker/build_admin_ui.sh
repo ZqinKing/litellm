3f70f715ff41d3254ced7d70e37f8b1c4768b300
ZqinKing <zqinking23@gmail.com>
2026-03-10 10:43:22 +0800

    fix(docker): always rebuild default dashboard UI during image builds

--- docker/build_admin_ui.sh
+++ docker/build_admin_ui.sh
@@ -8,13 +8,12 @@ echo
 pwd
 
 
-# only run this step for litellm enterprise, we run this if enterprise/enterprise_ui/_enterprise.json exists
-if [ ! -f "enterprise/enterprise_ui/enterprise_colors.json" ]; then
-    echo "Admin UI - using default LiteLLM UI"
-    exit 0
+if [ -f "enterprise/enterprise_ui/enterprise_colors.json" ]; then
+    echo "Building custom admin UI..."
+    cp enterprise/enterprise_ui/enterprise_colors.json ui/litellm-dashboard/ui_colors.json
+else
+    echo "Building default LiteLLM UI..."
 fi
-
-echo "Building Custom Admin UI..."
 
 # Install dependencies
 # Check if we are on macOS
@@ -45,9 +44,6 @@ source ~/.nvm/nvm.sh
 nvm install v18.17.0
 nvm use v18.17.0
 npm install -g npm
-
-# copy _enterprise.json from this directory to /ui/litellm-dashboard, and rename it to ui_colors.json
-cp enterprise/enterprise_ui/enterprise_colors.json ui/litellm-dashboard/ui_colors.json
 
 # cd in to /ui/litellm-dashboard
 cd ui/litellm-dashboard
